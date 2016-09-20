a406de571fcd492ac04e6bb0eeab082383661a04
Almir <almir@alcassoft.com>
2016-09-20 12:52:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var core = require("brandchat-bot-core");
+
+//Initiate and connect to the Brandchat server
+var messageManager = new core.MessageManager();
+var brandchatManager = new core.BrandchatManager({bot_key:"your_bot_key", secret_key:"secret_key"});
+
+brandchatManager.connect(function(loginInfo){}, messageManager);
+
+//Welcome message from the Bot
+messageManager.welcomeMessage = function(question){
+   messageManager.sendMessage(question.id, "Hi. I'm Startup Bot");
+};
+
+//Bot correspondence flow
+messageManager.onMessage(function(message){
+   if(message.text=="hello"){
+      messageManager.sendMessage(message.questionId, "Hello to you too");   
+   }
+});
+
+
